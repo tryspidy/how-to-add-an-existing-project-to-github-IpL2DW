04588a159ee7bea6ab16c66858d11d84e28d106a
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-09 12:33:54 +0530

    Initial commit

---
+++ main.sh
@@ -0,0 +1,11 @@
+# Go into your existing directory and run below commands
+cd docker-spring-boot
+echo "# docker-spring-boot" >> README.md
+git init
+git add -A
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/devopsmaster/docker-spring-boot.git
+git push -u origin master
+                
+
